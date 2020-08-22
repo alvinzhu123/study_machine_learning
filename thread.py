9bb5d80ecf15ec262956b83c37f1eb4ad9cc1971
Alvin Zhu <xyzhuning@126.com>
2020-08-22 18:01:20 +0800

    add lock

--- thread.py
+++ thread.py
@@ -9,12 +9,31 @@ def thread_job():
         time.sleep(0.1)
     print("T1 stop\n")
 
+def job1():
+    global A, lock
+    lock.acquire()
+    for _ in range(10):
+        A += 1
+        print("job1", A)
+    lock.release()
+
+def job2():
+    global A, lock
+    lock.acquire()
+    for _ in range(10):
+        A += 10
+        print("job2", A)
+    lock.release()
+
 #python中同类型的数据，多线程并不一定是高效的，因为GIL的缘故，会锁住其他线程，实际在跑的仍然是单线程。
-def main():
-    added_thread = threading.Thread(target=thread_job(), name="T1")
-    added_thread.start()
-    added_thread.join()
-    print("all done\n")
+
 
 if __name__ == '__main__':
-    main()
+    lock = threading.Lock()
+    A = 0
+    t1 = threading.Thread(target=job1, name='t1')
+    t2 = threading.Thread(target=job2, name='t2')
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
