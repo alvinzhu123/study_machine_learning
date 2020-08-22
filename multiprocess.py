6380af6e265eef572f62084521abddea8e63f2b8
Alvin Zhu <xyzhuning@126.com>
2020-08-22 19:08:37 +0800

    multi_process

--- multiprocess.py
+++ multiprocess.py
@@ -1,4 +1,5 @@
 import multiprocessing as mp
+import time
 
 def job(q):
     res = 0
@@ -6,14 +7,33 @@ def job(q):
         res += i**2
     q.put(res)
 
+def job_core(x):
+    return x**2
+
+def multi_core():
+    pool = mp.Pool()
+    res = pool.map(job_core, range(10))
+    print(res)
+
+def lock_job(v, num, l):
+    l.acquire()
+    for _ in range(10):
+        time.sleep(0.1)
+        v.value += num
+        print(v.value)
+    l.release()
+
+
 if __name__ == '__main__':
-    q = mp.Queue()
-    p1 = mp.Process(target=job, args=(q,))
-    p2 = mp.Process(target=job, args=(q,))
+    # q = mp.Queue()
+    l = mp.Lock()
+    v = mp.Value('i', 0) #共享内存
+    p1 = mp.Process(target=lock_job, args=(v, 1, l))
+    p2 = mp.Process(target=lock_job, args=(v, 3, l))
     p1.start()
     p2.start()
     p1.join()
     p2.join()
-    res1 = q.get()
-    res2 = q.get()
-    print(res1+res2)
+    # res1 = q.get()
+    # res2 = q.get()
+    # multi_core()
